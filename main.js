11e04de83f5d646d9c22716f7930373d97bbcebe
BuildTools <unconfigured@null.spigotmc.org>
2024-03-20 17:01:31 -0700

    Main Function

--- main.js
+++ main.js
@@ -25,3 +25,26 @@ monsterReady = true;
 };
 monsterImage.src = "images/monster.png";
 
+var then = Date.now();
+//reset();
+ // call the main game loop.
+// The main game loop
+var main = function () {
+render();
+// Request to do this again ASAP using the Canvas method,
+// it’s much like the JS timer function “setInterval, it will
+// call the main method over and over again so our players
+// can move and be re-drawn
+requestAnimationFrame(main);
+};
+//To continuously call the main game loop function, this tutorial used to execute
+//the setInterval method. These days there's a better way, via
+//the requestAnimationFrame method.
+// Draw everything in the main render function
+var render = function () {
+if (bgReady) {
+
+ctx.drawImage(bgImage, 0, 0);
+}};
+
+main();
