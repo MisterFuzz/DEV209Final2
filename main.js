e44bbc89023dedaa51262a548c0438f471c796af
BuildTools <unconfigured@null.spigotmc.org>
2024-03-21 20:21:26 -0700

    Game Complete

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ canvas.id = "Game";
 var ctx = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 659;
-var countDown = 3;
+var countDown = 5;
+var score = 0;
 var continueGame = true;
 document.body.appendChild(canvas);
+
+var audio = new Audio('sounds/splash.ogg');
+var audio2 = new Audio('sounds/explode1.ogg');
+var audio3 = new Audio('sounds/lavapop.ogg');
 
 var bgReady = false;
 var bgImage = new Image();
@@ -43,6 +48,20 @@ gasImage.onload = function () {
 };
 gasImage.src = "images/gas2.png";
 
+var orbReady = false;
+var orbImage = new Image();
+orbImage.onload = function () {
+    orbReady = true;
+};
+orbImage.src = "images/orb.png";
+
+holeReady = false;
+var holeImage = new Image();
+holeImage.onload = function () {
+    holeReady = true;
+};
+holeImage.src = "images/hole.png";
+
 var car = {
     xSpeed: 10, // movement in pixels per second
     ySpeed: 10,
@@ -54,7 +73,15 @@ var gas = {
     x: 0,
     y: 0
 };
-
+var orb = {
+    // for this version, the gas does not move, so just and x and y
+    x: 0,
+    y: 0
+};
+var hole = {
+    x: 0,
+    y: 0
+};
 
 var keysDown = {}; //object were we properties when keys go down
                 // and then delete them when the key goes up
@@ -96,6 +123,13 @@ var rotateDraw = function () {
     }
 }
 
+var gameOver = function () {
+    document.getElementById("Game").remove();
+    var endMessage = document.createElement("h1");
+    endMessage.innerText = "YOU DIED";
+    document.body.appendChild(endMessage);
+};
+
 var update = function (modifier) {
     if (
         car.x <= (gas.x + 64)
@@ -104,7 +138,30 @@ var update = function (modifier) {
         && gas.y <= (car.y + 32)
     ) {
         countDown = 5;
+        audio3.play();
         reset();       // start a new cycle
+    }
+    if (
+        car.x <= (orb.x + 64)
+        && orb.x <= (car.x + 64)
+        && car.y <= (orb.y + 32)
+        && orb.y <= (car.y + 32)
+    ) {
+        var audio = new Audio('sounds/successful_hit.ogg');
+        audio.play();
+        score += 1;
+        reset();      // start a new cycle
+    }
+
+    if (
+        car.x <= (orb.x + 64)
+        && hole.x <= (car.x + 64)
+        && car.y <= (hole.y + 32)
+        && hole.y <= (car.y + 32)
+    ) {
+        var audio = new Audio('sounds/splash.ogg');
+        audio.play();
+        gameOver();       // start a new cycle
     }
 
     if (38 in keysDown) { //  holding up key
@@ -155,15 +212,17 @@ var update = function (modifier) {
 };
 
 
-var reset = function () {
-    // car.x = canvas.width / 2;
-    // car.y = canvas.height / 2;
-
+var reset = function (player = false) {
 //Place the gas somewhere on the screen randomly
 // but not in the hedges, Article in wrong, the 64 needs to be 
 // hedge 32 + hedge 32 + char 32 = 96
     gas.x = 32 + (Math.random() * (canvas.width - 96));
     gas.y = 32 + (Math.random() * (canvas.height - 96));
+    hole.x = 32 + (Math.random() * (canvas.width - 96));
+    hole.y = 32 + (Math.random() * (canvas.height - 96));
+    orb.x = 32 + (Math.random() * (canvas.width - 96));
+    orb.y = 32 + (Math.random() * (canvas.height - 96));
+
 };
 
 var then = Date.now();
@@ -177,10 +236,7 @@ var main = function () {
     render();
     then = now;
     if (!continueGame) {
-        document.getElementById("Game").remove();
-        var endMessage = document.createElement("h1");
-        endMessage.innerText = "YOU DIED";
-        document.body.appendChild(endMessage);
+        gameOver();
     }
     else {
         requestAnimationFrame(main);
@@ -194,19 +250,25 @@ var render = function () {
     if (bgReady) {
         ctx.drawImage(bgImage, 0, 0);
     }
-    if (true) {
+    if (carReady == 4) {
         rotateDraw();
     }
     if (gasReady) {
         ctx.drawImage(gasImage, gas.x, gas.y);
+    }
+    if (holeReady) {
+        ctx.drawImage(holeImage, hole.x, hole.y);
+    }
+    if (orbReady) {
+        ctx.drawImage(orbImage, orb.x, orb.y);
     }
 
     ctx.fillStyle = "rgb(250, 250, 250)";
     ctx.font = "24px Helvetica";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
-    ctx.fillText("Time until Death: " + countDown, 32, 32);
-
+    ctx.fillText("Fuel up or explode: " + countDown, 32, 32);
+    ctx.fillText("Points: " + score, 32, 96);
 };
 
 
@@ -215,5 +277,6 @@ window.setInterval(function() {
     countDown -= 1;
     if (countDown == 0) {
         continueGame = false;
+        audio2.play();
     }
 }, 1000)
